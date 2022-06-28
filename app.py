f94c2f253f814146b9949b0be4ccca709795c8be
yanivniv <yanivniv@gmail.com>
2022-06-28 11:09:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,3 @@
-# https://ngugijoan.medium.com/deploy-machine-learning-web-applications-with-streamlit-238b0380679d
-# https://github.com/snehankekre/streamlit-shap
-
-
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import pickle
-
-
-
-# elif app_mode == "Predict Airbnb Price":
-# ## specify our inputs
-#     st.subheader('Fill in employee details to get prediction ')
-#     st.sidebar.header("Other details :")
-#     prop = {'salary_low': 1, 'salary_high': 2, 'salary_medium': 3}
-#     satisfaction_level = st.number_input("satisfaction_level", min_value=0.0, max_value=1.0)
-#     average_montly_hours = st.number_input("average_montly_hours")
-#     promotion_last_5year = st.number_input("promotion_last_5year")
-#     salary = st.sidebar.radio("Select Salary ",tuple(prop.keys()))
-
-#     salary_low,salary_medium,salary_high=0,0,0
-#     if salary == 'High':
-#         salary_high = 1
-#     elif salary == 'Low':
-#         salary_low = 1
-#     else :salary_medium = 0
-
-
-#     subdata={
-#         'satisfaction_level':satisfaction_level,
-#         'average_montly_hours ':average_montly_hours ,
-#         'promotion_last_5year':   promotion_last_5year,
-#         'salary':[salary_low,salary_medium,salary_high],
-#         }
-
-#     features = [satisfaction_level, average_montly_hours, promotion_last_5year, subdata['salary'][0],subdata['salary'][1], subdata['salary'][2]]
-
-#     results = np.array(features).reshape(1, -1)
-
-
-
 import streamlit as st
 st.set_page_config(layout="wide")
 from streamlit_shap import st_shap
@@ -51,21 +9,26 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 #from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-
+path_var = "https://github.com/yanivniv/streamlit/blob/main/"
 
 @st.experimental_memo
 def load_data():
-    df = pd.read_csv('c:\castpone\df_final.csv')
-    X = pd.read_csv('c:\castpone\X_new.csv')                
-    picklefile = open("c:/castpone/top_10_model.pkl", "rb")
+    #df = pd.read_csv('c:\castpone\df_final.csv')
+    df = pd.read_csv('https://github.com/yanivniv/streamlit/blob/main/df_final.csv')
+    #X = pd.read_csv('c:\castpone\X_new.csv')                
+    X = pd.read_csv('https://github.com/yanivniv/streamlit/blob/main/X_new.csv')                
+    #picklefile = open("c:/castpone/top_10_model.pkl", "rb")
+    picklefile = open("https://github.com/yanivniv/streamlit/blob/main/top_10_model.pkl", "rb")
     model = pickle.load(picklefile)       
     return df,model,X
 
 @st.experimental_memo
 def load_model():
-    df = pd.read_csv('c:\castpone\df_final.csv')
+    #df = pd.read_csv('c:\castpone\df_final.csv')
+    df = pd.read_csv('https://github.com/yanivniv/streamlit/blob/main/df_final.csv')
     #X = pd.read_csv('c:\castpone\X_new.csv')                
-    picklefile = open("c:/castpone/top_10_model.pkl", "rb")
+    #picklefile = open("c:/castpone/top_10_model.pkl", "rb")
+    picklefile = open("https://github.com/yanivniv/streamlit/blob/main/top_10_model.pkl", "rb")
     model = pickle.load(picklefile)       
     return df,model
 
